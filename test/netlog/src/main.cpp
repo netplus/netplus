24821dc2a5cad9a385d253d7ff006f4add0844a8
cao ping <p.cao@outlook.com>
2021-01-31 15:06:01 +0800

    finish netlog

--- test/netlog/src/main.cpp
+++ test/netlog/src/main.cpp
@@ -7,15 +7,15 @@ int main(int argc, char** argv) {
 
 	netp::rpc::listen(log_server, [](NRP<netp::rpc> const& rpc_) {
 		rpc_->on_push([](NRP<netp::rpc> const& rpc_, NRP<netp::packet> const& in) {
-			printf("[netlog]<<\n%s-----\n", std::string((char*)in->begin(), in->len()).c_str() );
+			printf("[netlog]<<\n%s-----\n", std::string((char*)in->head(), in->len()).c_str() );
 		});
 	});
 
-	NRP<netp::log::net_logger> nlogger = netp::make_ref<netp::log::net_logger>(log_server);
-	netp::log::logger_manager::instance()->add_logger(nlogger);
+	NRP<netp::logger::net_logger> nlogger = netp::make_ref<netp::logger::net_logger>(log_server);
+	netp::logger_broker::instance()->add(nlogger);
 	NRP<netp::promise<int>> drp = nlogger->dial();
 	if (drp->get() != netp::OK) {
-		netp::log::logger_manager::instance()->remove_logger(nlogger);
+		netp::logger_broker::instance()->remove(nlogger);
 		NETP_ERR("dail log server failed");
 		return -1;
 	}
@@ -23,9 +23,8 @@ int main(int argc, char** argv) {
 	for (int i = 0; i < 5; ++i) {
 		NETP_INFO("test net logger");
 	}
-	netp::log::logger_manager::instance()->remove_logger(nlogger);
+	netp::logger_broker::instance()->remove(nlogger);
 
 	_app.run();
-	netp::log::logger_manager::instance()->remove_logger(nlogger);
 	return 0;
 }
