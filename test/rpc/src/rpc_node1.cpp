099e09161cce55cd17a4c4da484e4dd2a6288d8c
cao ping <p.cao@outlook.com>
2021-05-21 13:48:50 +0800

    simplify rpc exmaple

--- test/rpc/src/rpc_node1.cpp
+++ test/rpc/src/rpc_node1.cpp
@@ -2,121 +2,8 @@
 #include <netp.hpp>
 #include "shared.hpp"
 
-long long g_total_rpc;
-std::atomic<long long> g_rpc_count_now;
-long long g_total;
-long long g_message_size;
-std::atomic<long long> g_req_count;
-std::atomic<long long> g_resp_count;
-std::atomic<int> g_signal_switch;
-
-class foo :
-	public netp::ref_base
-{
-public:
-	void ping(NRP<netp::rpc> const& r, NRP<netp::packet> const& in, NRP<netp::rpc_call_promise> const& f) {
-		NRP<netp::packet> pong = netp::make_ref<netp::packet>(in->head(), in->len());
-		f->set(std::make_tuple(netp::OK, pong));
-	}
-
-	void on_push(NRP<netp::rpc> const& r, NRP<netp::packet> const& in) {
-		NRP<netp::promise<int>> replyp =r->push(netp::make_ref<netp::packet>(in->head(), in->len()));
-		replyp->if_done([]( int const& rt ) {
-			NETP_ASSERT(rt == netp::OK);
-		});
-	}
-};
-
-struct benchmark {
-	std::chrono::time_point<std::chrono::steady_clock, std::chrono::steady_clock::duration> begin;
-	std::string m_tag;
-	benchmark(std::string const& tag) : m_tag(tag),
-		begin(std::chrono::steady_clock::now())
-	{
-	}
-
-	~benchmark() {
-		std::chrono::time_point<std::chrono::steady_clock, std::chrono::steady_clock::duration> end = std::chrono::steady_clock::now();
-		NETP_INFO("[%s][end]cost: %lld micro second", m_tag.c_str(), ((end - begin).count() / 1000));
-	}
-
-	std::chrono::steady_clock::duration mark(std::string const& tag) {
-		std::chrono::time_point<std::chrono::steady_clock, std::chrono::steady_clock::duration> end = std::chrono::steady_clock::now();
-		NETP_INFO("[%s][%s]cost: %lld micro second", m_tag.c_str(), tag.c_str(), ((end - begin).count() / 1000));
-		return end-begin;
-	}
-};
-
-void _client_on_push(NRP<netp::rpc> const& r, NRP<netp::packet> const& inp) {
-	//NETP_ASSERT( inp->len() == 256*1024 );
-	if (netp::atomic_incre(&g_resp_count) == g_total-1) {
-		::raise(SIGINT);
-	}
-}
-
-void push(NRP<netp::rpc> const& r , NRP<netp::packet> const& outp ) {
-	if ( netp::atomic_incre(&g_req_count) != g_total) {
-		NRP<netp::promise<int>> pushp = netp::make_ref<netp::promise<int>>();
-		pushp->if_done([r, outp](int const& rt) {
-			if (rt == netp::OK) {
-				push(r, outp);
-			}
-		});
-		r->do_push(pushp,outp);
-	}
-}
-
-void call(NRP<netp::rpc> const& r, NRP<netp::packet> const& outp, long long rc) {
-
-	NRP<netp::rpc_call_promise> cp = netp::make_ref<netp::rpc_call_promise>();
-	cp->if_done([outp, r,nrc=rc+1](std::tuple<int, NRP<netp::packet>> const& tupp) {
-		int rt = std::get<0>(tupp);
-		if (rt != netp::OK) {
-			r->close();
-			return;
-		}
-		long long lastc = netp::atomic_incre(&g_resp_count);
-		if (lastc >= g_total) {
-			NETP_DEBUG("test done, rc: %lld", nrc );
-			r->close();
-
-			if (g_total_rpc == g_rpc_count_now) {
-				if (netp::atomic_incre(&g_signal_switch) == 0) {
-					::raise(SIGINT);
-				}
-			}
-			return;
-		}
-
-		NETP_ASSERT(std::get<1>(tupp)->len() == outp->len());
-		call(r, outp,nrc);
-	});
-	r->call(cp, rpc_call_test_api::API_PING, outp );
-}
-
 int main(int argc, char** argv) {
-	std::shared_ptr<netp::app> _app = std::make_shared<netp::app>();
-
-	g_total = 10000;
-	g_message_size = 64;
-	g_total_rpc = 1;
-	g_req_count = 0;
-	g_resp_count = 0;
-	g_rpc_count_now = 0;
-	g_signal_switch = 0;
-
-	if (argc == 2) {
-		g_total = netp::to_i64(argv[1]);
-	}
-	else if (argc == 3) {
-		g_total = netp::to_i64(argv[1]);
-		g_message_size = netp::to_i64(argv[2]);
-	}
-	else if (argc == 4) {
-		g_total = netp::to_i64(argv[1]);
-		g_message_size = netp::to_i64(argv[2]);
-		g_total_rpc = netp::to_i32(argv[3]);
-	}
+	netp::app  _app;
 
 	netp::fn_rpc_activity_notify_t fn_bind_api = [](NRP<netp::rpc> const& r) {
 		r->bindcall(rpc_call_test_api::API_PING, 
@@ -135,48 +22,29 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 
-	benchmark bhm("test rpc call");
-
-	NRP<netp::rpc>* rpcs = new NRP<netp::rpc>[g_total_rpc];
-	for (int i = 0; i < g_total_rpc; ++i) {
-		NRP<netp::packet> outp = netp::make_ref<netp::packet>(g_message_size);
-		outp->incre_write_idx(g_message_size);
-		NRP<netp::rpc_dial_promise> rdp = netp::rpc::dial("tcp://127.0.0.1:21001", nullptr, cfg);
-		rdp->if_done([outp, rpcs,i](std::tuple<int, NRP<netp::rpc>> const& tupr) {
-			if (std::get<0>(tupr) != netp::OK) {
-				NETP_WARN("dial failed, , failed rt: %d", std::get<0>(tupr));
-				return;
-			}
-
-			NRP<netp::rpc> r = std::get<1>(tupr);
-			netp::atomic_incre(&g_rpc_count_now);
-			*(rpcs + i) = r;
-			call(r, outp,0);
-		});
-		if ((i % 200) == 0) {
-			rdp->wait();
+	NRP<netp::rpc_dial_promise> rdp = netp::rpc::dial("tcp://127.0.0.1:21001", nullptr, cfg);
+	rdp->if_done([](std::tuple<int, NRP<netp::rpc>> const& tupr) {
+		if (std::get<0>(tupr) != netp::OK) {
+			NETP_WARN("dial failed, , failed rt: %d", std::get<0>(tupr));
+			return;
 		}
-	}
 
-	_app->run();
-	bhm.mark("wait rpc");
-	for (int i = 0; i < g_total_rpc; ++i) {
-		(*(rpcs + i))->close_promise()->wait();
-		(*(rpcs + i)) = nullptr;
-	}
+		NRP<netp::packet> outp = netp::make_ref<netp::packet>(128);
+		const char* hello_rpc = "hello rpc";
+		outp->write(hello_rpc, netp::strlen(hello_rpc));
+		NRP<netp::rpc> r = std::get<1>(tupr);
+		NRP<netp::rpc_call_promise> rcp = r->call(rpc_call_test_api::API_PING, outp);
 
-	std::chrono::steady_clock::duration total_dur = bhm.mark("all done");
-	std::chrono::seconds sec = std::chrono::duration_cast<std::chrono::seconds>(total_dur);
-	NETP_INFO("cost seconds: %lld", sec.count() );
-	if (sec.count() == 0) {
-		sec = std::chrono::seconds(1);
-	}
+		rcp->if_done([](std::tuple<int, NRP<netp::packet>> const& tupp) {
+			int rt = std::get<0>(tupp);
+			NRP<netp::packet> rpc_reply = std::get<1>(tupp);
 
-	double rate = (g_total*1.0) / sec.count();
-	double thp = (g_total*g_message_size*1.0) / (sec.count()*1000);
+			NETP_INFO("rpc reply: %s", std::string((char*)rpc_reply->head(), rpc_reply->len()).c_str());
+			::raise(SIGINT);
+		});
+	});
 
-	NETP_INFO("total: %u, cost seconds: %lld, rate: %0.f/s, thp: %0.f kb/s", g_total, sec.count(), rate, thp);
-
+	_app.run();
 	std::get<1>(rpc_lf->get())->ch_close();
 
 	return 0;
