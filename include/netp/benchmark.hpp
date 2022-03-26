7f20d2a13eb070f36caa12afedd0c7598aebd16e
bingbing <p.cao@outlook.com>
2022-03-26 16:58:43 +0800

    opt benchmark

--- include/netp/benchmark.hpp
+++ include/netp/benchmark.hpp
@@ -7,19 +7,25 @@
 #include <netp/app.hpp>
 
 namespace netp {
+	enum benchmark_flag {
+		bf_no_end_output = 1<<0,
+		bf_no_mark_output =1<<1,
+		bf_force_us = 1<<2	
+	};
 	struct benchmark {
-		bool no_output;
+		int flag;
 		std::string m_tag;
 		std::chrono::time_point<std::chrono::steady_clock, std::chrono::steady_clock::duration> begin;
-		benchmark(std::string const& tag, bool no_output_ = false ) : 
-			no_output(no_output_),
-			m_tag(tag), begin(std::chrono::steady_clock::now())
+		benchmark(std::string const& tag, int flag_ = bf_no_end_output ) :
+			flag(flag_),
+			m_tag(tag),
+			begin(std::chrono::steady_clock::now())
 		{
 		}
 
 		~benchmark() {
 			std::chrono::time_point<std::chrono::steady_clock, std::chrono::steady_clock::duration> end = std::chrono::steady_clock::now();
-			if (!no_output) { NETP_INFO("[%s][end]cost: %lld us", m_tag.c_str(), ((end - begin).count() / 1000)); }
+			if (!(flag&bf_no_end_output)) { NETP_INFO("[%s][end]cost: %lld %s", m_tag.c_str(), ((end - begin).count() / ( (flag&bf_force_us) ?1000:1)), (flag & bf_force_us) ? "us" : "ns"); }
 		}
 
 		inline std::chrono::steady_clock::duration elapsed() const {
@@ -28,8 +34,8 @@ namespace netp {
 
 		std::chrono::steady_clock::duration mark(std::string const& tag) {
 			std::chrono::steady_clock::duration elapsed_ = elapsed();
-			if (!no_output) {
-				NETP_INFO("[%s][%s]cost: %lld us", m_tag.c_str(), tag.c_str(), ((elapsed_).count() / 1000));
+			if (!(flag&bf_no_mark_output)) {
+				NETP_INFO("[%s][%s]cost: %lld %s", m_tag.c_str(), tag.c_str(), ((elapsed_).count() / ((flag & bf_force_us) ? 1000 : 1) ), (flag & bf_force_us) ? "us":"ns");
 			}
 			return elapsed_;
 		}
