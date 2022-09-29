8c22af1d6202274df6f521e809c61fa21a4895cb
netplus <netplusplus@outlook.com>
2022-09-29 15:40:45 +0800

    add linux makefile

--- test/condition_cost/src/main.cpp
+++ test/condition_cost/src/main.cpp
@@ -1,50 +1,50 @@
 #include <netp.hpp>
 #include <iostream>
 
-struct non_pod {
-	int i;
-	non_pod(int i_) :
-		i(i_)
-	{}
-	virtual ~non_pod() 
-	{
-		i = 3;
-	}
+std::condition_variable g_cv;
+std::mutex g_mtx;
+
+struct cost_sample {
+	//in nanos
+	long long begin;
+	long long end;
 };
-
-NSP<std::vector<NSP<non_pod>>> g__xx ;
-
-
-void th_run() {
-	std::atomic_thread_fence(std::memory_order_acquire);
-	NSP<std::vector<NSP<non_pod>>> __g_copy = g__xx;
-	g__xx=(nullptr);
-	std::atomic_thread_fence(std::memory_order_release);
-	int i = 0;
-	NSP<non_pod> nnpod = netp::make_shared<non_pod>(0);
-	__g_copy->push_back(nnpod);
-
-	nnpod = nullptr;
+cost_sample g_tmp;
+netp::benchmark g_mk("cost_sample");
+void th_wait() {
 	while (1) {
-		std::cout << i++ << std::endl;	
-		netp::this_thread::sleep_for(std::chrono::seconds(1));
+		std::unique_lock<std::mutex> ulk(g_mtx);
+		g_cv.wait(ulk);
+		g_tmp.end = g_mk.mark("waken").count();
+		NETP_INFO("[condition_cost]cost: %lld ns", (g_tmp.end - g_tmp.begin));
+		netp::this_thread::yield();
 	}
-
-	__g_copy = nullptr;
+}
+void th_notify() {
+	while (1) {
+		g_tmp.begin = g_mk.mark("notify").count();
+		std::unique_lock<std::mutex> ulk(g_mtx);
+		g_cv.notify_one();
+		//netp::this_thread::usleep(netp::random(100,1000));
+	}
 }
 
 int main(int argc, char** argv) {
 	netp::app::instance()->init(argc,argv);
 	netp::app::instance()->start_loop();
-	g__xx = netp::make_shared<std::vector<NSP<non_pod>>>();
-	std::atomic_thread_fence(std::memory_order_release);
-	NRP<netp::thread> th1 = netp::make_ref<netp::thread>();
-	th1->start(&th_run);
 
-	NRP<netp::timer> tm = netp::make_ref<netp::timer>(std::chrono::seconds(5), [th1](NRP<netp::timer> const& tm) {
-		th1->interrupt();
-	});
-	netp::app::instance()->def_loop_group()->launch(tm);
-//	th1->join();
+	NRP<netp::thread> thwait = netp::make_ref<netp::thread>();
+	thwait->start(&th_wait);
+
+	NRP<netp::thread> thnotify = netp::make_ref<netp::thread>();
+	thnotify->start(&th_notify);
+
+	netp::app::instance()->wait();
+
+	thwait->interrupt();
+	thnotify->interrupt();
+	thwait->join();
+	thnotify->join();
+
 	return 0;
 }
