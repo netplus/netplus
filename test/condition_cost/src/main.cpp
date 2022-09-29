aab7d5ccb92c864840c24c8abfa1f91b206aec59
bingbing <p.cao@outlook.com>
2022-09-29 19:03:08 +0800

    update condition_cost, set begin,end to atomic type

--- test/condition_cost/src/main.cpp
+++ test/condition_cost/src/main.cpp
@@ -6,8 +6,8 @@ std::mutex g_mtx;
 
 struct cost_sample {
 	//in nanos
-	long long begin;
-	long long end;
+	std::atomic<long long> begin;
+	std::atomic<long long> end;
 };
 cost_sample g_tmp;
 netp::benchmark g_mk("cost_sample", netp::bf_no_mark_output|netp::bf_no_end_output);
@@ -15,14 +15,14 @@ void th_wait() {
 	while (1) {
 		std::unique_lock<std::mutex> ulk(g_mtx);
 		g_cv.wait(ulk);
-		g_tmp.end = g_mk.mark("waken").count();
+		g_tmp.end.store(g_mk.mark("waken").count(), std::memory_order_relaxed);
 		NETP_INFO("[condition_cost]cost: %lld ns", (g_tmp.end - g_tmp.begin));
 		netp::this_thread::yield();
 	}
 }
 void th_notify() {
 	while (1) {
-		g_tmp.begin = g_mk.mark("notify").count();
+		g_tmp.begin.store(g_mk.mark("notify").count(), std::memory_order_relaxed);
 		std::unique_lock<std::mutex> ulk(g_mtx);
 		g_cv.notify_one();
 		netp::this_thread::yield();
