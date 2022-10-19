54778b77c0e94ba681e38a4c17040de56570f7b7
bingbing <p.cao@outlook.com>
2022-10-19 14:46:42 +0800

    patch for armv7a

--- include/netp/core/platform/gnu_linux_.hpp
+++ include/netp/core/platform/gnu_linux_.hpp
@@ -2,6 +2,8 @@
 #ifndef _CORE_PLATFORM_NETP_PLATFORM_GNU_LINUX_HPP_
 #define _CORE_PLATFORM_NETP_PLATFORM_GNU_LINUX_HPP_
 
+//@note: DO NOT INCLUDE linux/*.h from user space app directlyl, IIF YOU KNOW WHAT YOU ARE DOING
+// if there is a missing MACRO in netinet/x.h FILE, but it do exists in linux/x.h for the target host kernel, we could define that MACRO by mannual
 //for gnu linux
 #include <sys/time.h>
 #include <sys/types.h>
@@ -10,19 +12,19 @@
 #include <errno.h>
 #include <stdio.h>
 
+#include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
 #include <sys/socket.h>
-#include <fcntl.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
 #include <sys/un.h> //sockaddr_un
 #include <sys/uio.h>
 #include <sys/ioctl.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 #include <linux/version.h>
-
+//SO_INCOMING_CPU (gettable since Linux 3.19, settable since Linux 4.4)
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,4,0)
 	#define __NETP_ENABLE_SO_INCOMING_CPU
 #endif
--- include/netp/core/platform/gnu_linux_armv7a_.hpp
+++ include/netp/core/platform/gnu_linux_armv7a_.hpp
@@ -1,4 +1,8 @@
 #ifndef _CORE_PLATFORM_NETP_PLATFORM_GNU_LINUX_ARMV7A_HPP_
 #define _CORE_PLATFORM_NETP_PLATFORM_GNU_LINUX_ARMV7A_HPP_
 
+#ifndef UDP_SEGMENT
+	//tested on pi4/armv7a/gcc 8.3
+	#define UDP_SEGMENT	103	/* Set GSO segmentation size */
+#endif
 #endif
